281ace1a3d1eea67fd9efa19a7dcc6bde30da182
shkurkoka <44337947+shkurkoka@users.noreply.github.com>
2023-06-08 19:33:48 +0000

    Deploying to gh-pages from @ shkurkoka/goit-js-hw-10@dd1896de1255a9147123795c067c98f3953e293d 🚀

---
+++ index.aa054d57.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".breed-select");document.querySelector(".cat-info");e.addEventListener("click",(()=>{analys().then().catch((e=>console.log(e)))}));
+//# sourceMappingURL=index.aa054d57.js.map
