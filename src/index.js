dd1896de1255a9147123795c067c98f3953e293d
shkurkoka <shkurkoka@gmail.com>
2023-06-08 22:33:27 +0300

    Update index.js

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const selector = document.querySelector('.breed-select');
+const listEl = document.querySelector('.cat-info');
+
+selector.addEventListener('click', () => {
+    analys()
+        .then()
+        .catch ((error) => console.log(error));
+
+});
+
+
+
+
+
+
+
+
