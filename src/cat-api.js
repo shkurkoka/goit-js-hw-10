f4d87d0f57752b2455cb027c520dc211554bc0ea
shkurkoka <shkurkoka@gmail.com>
2023-06-09 22:00:13 +0300

    second step

--- src/cat-api.js
+++ src/cat-api.js
@@ -3,13 +3,24 @@ export async function fetchBreeds(selector) {
         .then(response => response.json())
         .then(data => {
             data.forEach(breed => {
-                const option = document.createElement(`
+                selector.insertAdjacentHTML( "beforeend", `
                     <option value="${breed.id}">${breed.name}</option>
                 `);
-                selector.appendChild(option);
             });
         })
         .catch(erorr => console.log("Erorr", erorr));
 }
 
+export async function fetchCatByBreed(breedId) {
+    fetch(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
+        .then(response => response.json())
+        .then(data => {
+            data.forEach(breed => {
+                selector.insertAdjacentHTML( "beforeend", `
+                    <option value="${breed.id}">${breed.name}</option>
+                `);
+            });
+        })
+        .catch(erorr => console.log("Erorr", erorr));
+}
 
--- src/index.js
+++ src/index.js
@@ -1,21 +1,25 @@
-import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
+import { fetchBreeds} from './cat-api.js';
 
 const selector = document.querySelector('.breed-select');
 const listEl = document.querySelector('.cat-info');
+const options = document.querySelectorAll('option');
+fetchBreeds(selector);
 
 const APIkey = 'live_qYlL8o9MDIH8U2L6yURyex2Y5rzbgVNFDh0R2EHrDR6dYWpbJMuVjmE2sG4pixTn';
 
-fetchBreeds(selector);
 
-
-
-
-
-selector.addEventListener('click', () => {
-    fetchCatByBreed()
-        .then((posts) => renderPosts(posts))
-        .catch ((error) => console.log(error));
+selector.addEventListener('change', () => {
+    // console.log("Hi");
+    options.forEach(option => {
+        if (option.hasAttribute('checked')) {
+            console.log("option name", option.value);
+        }
+    });
 });
+
+
+
+
 
 
 
